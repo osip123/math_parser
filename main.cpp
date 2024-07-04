b976172959b52095bf5ae77044e9565aeb876cc9
osip123 <131646261+osip123@users.noreply.github.com>
2024-07-04 14:03:35 +0300

    add 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "src/"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
---
+++ src/tokens/tokens_list.h
@@ -0,0 +1,20 @@
+//
+// Created by endoc on 04.07.2024.
+//
+
+#ifndef MATH_PARSER_TOKENS_LIST_H
+#define MATH_PARSER_TOKENS_LIST_H
+
+enum class Tokens{
+    SIN,
+    COS,
+    TG,
+    CTG,
+    ARSIN,
+    ARCOS,
+    ARTG,
+    ARCTG,
+    UNNOWN,
+};
+
+#endif //MATH_PARSER_TOKENS_LIST_H
